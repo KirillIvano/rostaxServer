e2663811057d64f96ba1bc3897952e43b1a7160b
KirillIvano <ivannadez@yandex.ru>
2020-02-22 21:19:35 +0300

    sendFile

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(express.static(path.resolve(__dirname, 'static', 'dist')));
 const graphQlRoutes = require('./graph').initializeGraphql();
 
 app.post('/gql', graphQlRoutes);
-app.get('/', (req, res) => res.render('index.html'));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'static', 'dist', 'index.html')));
+
+
 const server = http.createServer(app);
 
 server.listen(process.env.PORT || 5000);
